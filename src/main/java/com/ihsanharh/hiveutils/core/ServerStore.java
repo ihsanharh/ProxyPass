f81870fb70d6c94edf21f1d68dc8f3a85c652c8d
XzFrosT <mschodeihsan@gmail.com>
2026-03-24 14:15:48 +0700

    fix not clearing players

--- src/main/java/com/ihsanharh/hiveutils/core/ServerStore.java
+++ src/main/java/com/ihsanharh/hiveutils/core/ServerStore.java
@@ -4,8 +4,6 @@ public class ServerStore {
     private static final ServerStore INSTANCE = new ServerStore();
     private String currentServerName = "UNKNOWN";
     private String previousServerName = "UNKNOWN";
-    private String serverType = "UNKNOWN";
-    private int serverNumber = -1;
 
     public static ServerStore getInstance() {
         return INSTANCE;
@@ -25,15 +23,6 @@ public class ServerStore {
         }
         this.previousServerName = this.currentServerName;
         this.currentServerName = serverName;
-
-        this.serverType = serverName.replaceAll("\\d", "");
-        String numPart = serverName.replaceAll("\\D", "");
-
-        if (!numPart.isEmpty()) {
-            this.serverNumber = Integer.parseInt(numPart);
-        } else {
-            this.serverNumber = -1;
-        }
 
         if (!this.previousServerName.equals("UNKNOWN")) {
             PlayerStore.getInstance().clear();
