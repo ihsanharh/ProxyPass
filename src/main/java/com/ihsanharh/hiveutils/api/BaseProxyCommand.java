d839937b2ecf9186fc9d14093022b837294f50e5
XzFrosT <mschodeihsan@gmail.com>
2026-04-07 02:38:51 +0700

    restructure command

---
+++ src/main/java/com/ihsanharh/hiveutils/api/BaseProxyCommand.java
@@ -0,0 +1,37 @@
+package com.ihsanharh.hiveutils.api;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.cloudburstmc.protocol.bedrock.data.command.ChainedSubCommandData;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandData;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandData.Flag;
+import org.cloudburstmc.protocol.bedrock.data.command.CommandOverloadData;
+import org.cloudburstmc.proxypass.network.bedrock.session.ProxyPlayerSession;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseProxyCommand implements ProxyCommand {
+    private final String name;
+    private final String description;
+
+    public BaseProxyCommand(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public CommandData buildCommandData() {
+        Set<Flag> commandFlags = new HashSet<>();
+        List<ChainedSubCommandData> subCommands = new ArrayList<>();
+        CommandOverloadData[] overloads = new CommandOverloadData[0];
+
+        return new CommandData(name, description, commandFlags, null, null, subCommands, overloads);
+    }
+
+    @Override
+    public abstract void execute(ProxyPlayerSession session, String[] args);
+}
--- src/main/java/com/ihsanharh/hiveutils/commands/FindCommand.java
+++ src/main/java/com/ihsanharh/hiveutils/commands/FindCommand.java
@@ -1,42 +1,18 @@
 package com.ihsanharh.hiveutils.commands;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.cloudburstmc.protocol.bedrock.data.command.ChainedSubCommandData;
-import org.cloudburstmc.protocol.bedrock.data.command.CommandData;
-import org.cloudburstmc.protocol.bedrock.data.command.CommandOverloadData;
-import org.cloudburstmc.protocol.bedrock.data.command.CommandData.Flag;
 import org.cloudburstmc.protocol.bedrock.packet.TextPacket;
 import org.cloudburstmc.proxypass.network.bedrock.session.ProxyPlayerSession;
 
-import com.ihsanharh.hiveutils.api.ProxyCommand;
+import com.ihsanharh.hiveutils.api.BaseProxyCommand;
 import com.ihsanharh.hiveutils.core.PlayerData;
 import com.ihsanharh.hiveutils.core.PlayerStore;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-public class FindCommand implements ProxyCommand {
-    @Override
-    public String getName() {
-        return "find";
-    }
-
-    @Override
-    public String getDescription() {
-        return "Finds a player's coordinates in the world";
-    }
-
-    @Override
-    public CommandData buildCommandData() {
-        Set<Flag> commandFlags = new HashSet<>();
-        List<ChainedSubCommandData> subCommands = new ArrayList<>();
-        CommandOverloadData[] overloads = new CommandOverloadData[0];
-
-        return new CommandData(this.getName(), this.getDescription(), commandFlags, null, null, subCommands, overloads);
+public class FindCommand extends BaseProxyCommand {
+    public FindCommand() {
+        super("find", "Finds a player's coordinates in the world");
     }
 
     @Override
